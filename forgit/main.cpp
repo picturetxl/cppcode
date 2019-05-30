467f465a0e18c60b390101896fa357cc607ccc76
liang tail <techtxl123@outlook.com>
2019-05-30 16:50:07 +0800

    pair back test

--- forgit/main.cpp
+++ forgit/main.cpp
@@ -1,14 +1,26 @@
 #include <iostream>
-
+#include <vector>
 #include <utility>
 
 using namespace std;
 
-
+pair<string, int> process(vector<string>& v)
+{
+	if (!v.empty())
+	{
+		return { v.back(),v.back().size() };
+	}
+	else
+	{
+		return pair<string, int>();
+	}
+}
 int main()
 {
 
-
+	vector<string> v = { "hello","world" };
+	pair<string, int> pa = process(v);
+	cout << pa.first << pa.second << endl;
 	cout << "test for vs git" << endl;
 	return 0;
 }
