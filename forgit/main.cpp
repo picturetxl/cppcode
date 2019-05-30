7e65a9e3ec595a664f8d678c371cb15a0f3c22d2
liang tail <techtxl123@outlook.com>
2019-05-30 17:44:12 +0800

    set iterator

--- forgit/main.cpp
+++ forgit/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <utility>
-
+#include <set>
 using namespace std;
 
 pair<string, int> process(vector<string>& v)
@@ -15,13 +15,27 @@ pair<string, int> process(vector<string>& v)
 		return pair<string, int>();
 	}
 }
-int main()
+void testPair()
 {
-
 	vector<string> v = { "hello","world" };
 	pair<string, int> pa = process(v);
 	cout << pa.first << pa.second << endl;
 	cout << "test for vs git" << endl;
+}
+int main()
+{
+	set<int> iset = { 0,1,2,3,4,5 };
+	set<int>::iterator set_it = iset.begin();//The two types of iterators of set are read-only
+	while (set_it!=iset.end())
+	{
+		if (*set_it == 3)
+		{
+			//*set_it = 9; //you cannot assign to a variable that is const
+			cout << "you cannot assign to a variable that is const" << endl;
+		}
+		cout << *set_it << endl;
+		++set_it;
+	}
 
 	return 0;
 }
